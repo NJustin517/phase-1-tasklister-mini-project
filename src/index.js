2c19c9d227008d635e2e2c4f4d8944f9c9d427cf
Justin Luciano <justinlluciano@gmail.com>
2021-12-20 00:39:37 -0800

    Assignment completed with 3 stretch deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const task = e.target.newTaskDescription.value;
+    const dueDate = e.target.newDueDate.value;
+    console.log(dueDate);
+    addTask(task, dueDate);
+    form.reset();
+  });
+
+  function addTask(task, dueDate) {
+    const li = document.createElement("li");
+    li.textContent = `${task}:  Due on ${dueDate} `;
+    const color = setPriorityLevel();
+    console.log(color);
+    li.style.color = color;
+    const button = document.createElement("button");
+    button.addEventListener("click", deleteButton);
+    button.textContent = "x";
+    li.appendChild(button);
+    document.querySelector("#tasks").appendChild(li);
+  }
+
+  function deleteButton(e) {
+    e.target.parentNode.remove();
+  }
+
+  function setPriorityLevel() {
+    const priorityLevel = document.querySelector("#dropDown").value;
+    if (priorityLevel === "highPriority") {
+      return "red";
+    } else if (priorityLevel === "mediumPriority") {
+      return "yellow";
+    } else if (priorityLevel === "lowPriority") {
+      return "green";
+    }
+  }
 });
